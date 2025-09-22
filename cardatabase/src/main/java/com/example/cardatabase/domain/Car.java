91ff401d84cd2690eabff561cad0324191ef4e39
cksdn1223 <rlacksdn1223@naver.com>
2025-09-22 17:05:58 +0900

    20250922 Spring Boot Lesson

---
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Car.java
@@ -0,0 +1,33 @@
+package com.example.cardatabase.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Entity
+@Data
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String brand, model, color, registrationNumber;
+
+    private int modelYear, price;
+
+    // JPA는 기본생성자가 필수적으로 요구됩니다.
+    public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.modelYear = modelYear;
+        this.price = price;
+    }
+
+}
---
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -0,0 +1,10 @@
+package com.example.cardatabase.domain;
+
+import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+public class Owner {
+
+}

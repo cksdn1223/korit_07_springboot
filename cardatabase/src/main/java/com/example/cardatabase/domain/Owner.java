2fffb0578426e2439a3c7a436f216d80b547e0a9
cksdn1223 <rlacksdn1223@naver.com>
2025-09-23 11:05:40 +0900

    feat: h2 인메모리 설정관련 코드

--- cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -1,10 +1,28 @@
 package com.example.cardatabase.domain;
 
-import jakarta.persistence.Entity;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-@NoArgsConstructor
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
 @Entity
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ownerId;
+
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+
+    // 소유자는 다수의 차들을 가질 수 있기 때문에 Collections 를 사용.
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Car> cars;
 
 }
--- cardatabase/src/main/java/test.java
+++ cardatabase/src/main/java/test.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class test {
+    public static void main(String[] args) {
+        String[] cards1 = {"i", "water", "drink"};
+        String[] cards2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+
+
+
+    }
 }

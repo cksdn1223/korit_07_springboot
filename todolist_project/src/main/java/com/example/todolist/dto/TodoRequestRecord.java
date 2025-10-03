28bba033844f85ed3471254720e7bfe5ecca287a
cksdn1223 <rlacksdn1223@naver.com>
2025-10-03 22:20:47 +0900

    메서드 JavaDoc 추가 및 로직 주석추가

--- todolist_project/src/main/java/com/example/todolist/dto/TodoRequestRecord.java
+++ todolist_project/src/main/java/com/example/todolist/dto/TodoRequestRecord.java
@@ -2,8 +2,4 @@ package com.example.todolist.dto;
 
 import com.example.todolist.entity.Todo;
 
-public record TodoRequestRecord(String content) {
-    public static TodoRequestRecord from(Todo todo) {
-        return new TodoRequestRecord(todo.getContent());
-    }
-}
+public record TodoRequestRecord(String content) {}
--- todolist_project/src/main/java/com/example/todolist/service/AppUserService.java
+++ todolist_project/src/main/java/com/example/todolist/service/AppUserService.java
@@ -22,15 +22,22 @@ public class AppUserService {
     private final PasswordEncoder passwordEncoder;
 
     public List<AppUserRecord> getAllUser() {
+        // 모든 유저를 찾고 record로 만들어 list형태로 리턴
         return appUserRepository.findAll().stream()
                 .map(appUser -> new AppUserRecord(appUser.getUsername(), appUser.getRole(), appUser.getTodos())).toList();
     }
 
     public ResponseEntity<AccountCredentialsRecord> saveUser(AccountCredentialsRecord request) {
+        // record로 입력받은 유저네임으로 유저를 찾음
         Optional<AppUser> searchUser = appUserRepository.findByUsername(request.username());
+        // 유저가 없어서 비어있다면
         if (searchUser.isEmpty()) {
+            // 회원가입 가능하니 유저를 저장 비밀번호 암호화 필수
             appUserRepository.save(new AppUser(request.username(), passwordEncoder.encode(request.password()), "USER"));
+            // 만들었으니 record와 HttpStatus를 CREATED로 리턴
             return new ResponseEntity<>(request, HttpStatus.CREATED);
-        } else throw new UsernameAlreadyExistsException("이미 존재하는 사용자입니다: " + request.username());
+        }
+        // searchUser가 비어있지 않다면 회원가입 불가능하니 예외처리
+        else throw new UsernameAlreadyExistsException("이미 존재하는 사용자입니다: " + request.username());
     }
 }
